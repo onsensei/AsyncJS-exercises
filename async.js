0c3bac105fbf47a6d20c94690ca49bff951e34ae
Thanaphat Suwannikornkul <onsensei@hotmail.com>
2017-11-21 11:05:28 +0700

    sync/async read file

--- async.js
+++ async.js
@@ -1,9 +1,30 @@
 const fs = require('fs');
 
-const readFromFile = (fileName)=>{
-  // WRITE CODE HERE
+//--------------------------------------------------
+
+const readFromFile = (fileName) => {
+    return fs.readFileSync(fileName, 'utf8');
+};
+
+// console.log('START');
+// const data = readFromFile('sample.txt');
+// console.log(data);
+// console.log('END');
+
+//--------------------------------------------------
+
+const readFromFileAsync = (fileName, callback) => {
+    fs.readFile(fileName, 'utf8', (err, data) => {
+        if(err) {
+            callback(true, err);
+            return;
+        }
+        callback(false, data);
+    });
 };
 
 console.log('START');
-const data = readFromFile('sample.txt');
-console.log('END')
+readFromFileAsync('sample.txt', (isError, data) => {
+    console.log(data);
+    console.log('END');
+});
