7278947b219ee96b007d247f815958ed8611bea1
Thanaphat Suwannikornkul <onsensei@hotmail.com>
2017-11-21 10:30:07 +0700

    init

--- async.js
+++ async.js
@@ -1,8 +1,13 @@
 const request = require('request');
 
+const callback = () => {
+    console.log('response');
+};
+
 const test = () => {
-  
-}
+    request('http://www.google.co.th', callback);
+};
+
 //START OF PROGRAM
 console.log("Hitting google");
 test();
